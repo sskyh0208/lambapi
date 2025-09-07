2d22511cb0d6486d8fcf14bfd8b5fc9890077159
GitHub Action <action@github.com>
2025-09-07 08:38:02 +0000

    chore: バージョンを v0.2.16 に更新

--- lambapi/__init__.py
+++ lambapi/__init__.py
@@ -51,7 +51,7 @@ except ImportError:
     _AUTH_AVAILABLE = False
     DynamoDBAuth = None  # type: ignore
 
-__version__ = "0.2.15"
+__version__ = "0.2.16"
 __author__ = "Your Name"
 __email__ = "your.email@example.com"
 
